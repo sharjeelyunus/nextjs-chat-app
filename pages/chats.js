e1786408a93a28268575c3b44d92e3cc26f6564a
sharjeelyunus <sharjeel924@gmail.com>
2022-01-31 02:57:30 +0500

    📦 NEW: Chats

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+)
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true)
+    }
+  });
+
+  useEffect(() => {
+    if (username.length === 0 || secret.length === 0) router.push("/")
+  })
+
+  if (!showChat) return <div />
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height='calc(100vh - 200px)'
+          projectID='8b801c23-831a-4d65-9616-1077f7eddc2f'
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
 }
